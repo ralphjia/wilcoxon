963d0d1b8352a01980d0418d37ba06aedac7e92a
Ralph <rliaoj@gmail.com>
2019-11-24 22:50:20 -0500

    added test cases

--- tests/testthat/test-wilcoxon_test.R
+++ tests/testthat/test-wilcoxon_test.R
@@ -37,11 +37,14 @@ test_that("Wilcoxon Rank-Sum test works", {
   expect_equal(wilcoxon_test(1:5, 0.5:8.5), wilcox.test(1:5, 0.5:8.5))
   expect_equal(wilcoxon_test(1:5, 0.5:8.5, alternative = "l"), wilcox.test(1:5, 0.5:8.5, alternative = "l"))
   expect_equal(wilcoxon_test(1:5, 0.5:8.5, alternative = "g"), wilcox.test(1:5, 0.5:8.5, alternative = "g"))
+  expect_equal(wilcoxon_test(0.5:8.5, 1:5), wilcox.test(0.5:8.5, 1:5))
   expect_equal(wilcoxon_test(-20:30, -25.5:20.5), wilcox.test(-20:30, -25.5:20.5))
   expect_equal(wilcoxon_test(-20:30, -25.5:20.5, alternative = "l"), wilcox.test(-20:30, -25.5:20.5, alternative = "l"))
   expect_equal(wilcoxon_test(-20:30, -25.5:20.5, alternative = "g"), wilcox.test(-20:30, -25.5:20.5, alternative = "g"))
 })
 
-
+test_that("Warning if exact is true but there are ties (for rank-sum test)", {
+  expect_warning(wilcoxon_test(1:5, 4:7, exact = T), "cannot compute exact p-value with ties")
+})
 
 
